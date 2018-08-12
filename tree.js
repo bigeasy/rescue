114663988b2459d77ca689b3266f03068d7d8514
Alan Gutierrez <alan@prettyrobots.com>
2018-08-12 01:28:50 -0500

    Only use cause if it is an actual `Error`.

--- tree.js
+++ tree.js
@@ -11,20 +11,8 @@ function tree (e, errors) {
     }
     var causes = Array.isArray(e.causes) ? e.causes : [ e.cause ]
     for (var i = 0, I = causes.length; i < I; i++) {
-        if (
-            causes[i] != null &&
-            typeof causes[i] == 'object' &&
-            ! Array.isArray(causes[i])
-        ) {
-            if (causes[i].message != null) {
-                node.causes.push(tree(causes[i], errors))
-            } else if (
-                causes[i].cause != null &&
-                ! Array.isArray(causes[i].cause) &&
-                causes[i].cause.message != null
-            ) {
-                node.causes.push(tree(causes[i].cause, errors))
-            }
+        if (causes[i] instanceof Error) {
+            node.causes.push(tree(causes[i], errors))
         }
     }
     return node
